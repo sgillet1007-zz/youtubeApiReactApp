290ff54d837fb7626c9bc2ed3cac6b5e65e46602
Samuel Gillet <samuel.gillet@ombud.com>
2016-03-21 22:44:18 -0600

    youtube player done

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,14 +9,16 @@ class SearchBar extends Component {
 
   render() {
     return (
-      <div>
+      <div className="search-bar">
         <input
           value ={this.state.term} // input is now a controlled element (value only changes when state changes)
-          onChange={event => this.setState({ term: event.target.value })}
-          />
+          onChange={event => this.onInputChange(event.target.value)} />
       </div>
-  )
-
+    );
+  }
+  onInputChange(term){
+    this.setState({term});
+    this.props.onSearchTermChange(term);
   }
 }
 
